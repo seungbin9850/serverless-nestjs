3f052e80db9b90fdf61b2d67b47415e09849915e
seungbin9850 <tanja1206@naver.com>
2021-08-31 14:42:30 +0900

    [ADD] user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import connectionOptions from '../ormconfig';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { UserModule } from './modules';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions)],
+  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
