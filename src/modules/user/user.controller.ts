ad9bda7eadb639e0fc72f84eeb535e192ac1a866
seungbin9850 <tanja1206@naver.com>
2021-09-01 19:12:26 +0900

    [ADD] create user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('/')
+  async createUser(@Body() req: CreateUserDTO) {
+    await this.userService.createUser(req);
+  }
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { HttpError } from 'src/exception';
+import { UserRepository } from 'src/repositories';
+import { hash, makeId } from 'src/utils';
 import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private userRepository: UserRepository) {}
+
+  async createUser(req: CreateUserDTO) {
+    const { userId, password } = req;
+    if (this.userRepository.findOne(userId))
+      throw new HttpError(409, 'Already Exist User ID');
+    const hashed = hash(password);
+    const id = await makeId();
+    const user = new User();
+    user.id = id;
+    user.userId = userId;
+    user.password = hashed;
+    await this.userRepository.save(user);
+  }
+}
