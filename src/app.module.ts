d4466cf7d89f269655d19751ede745c354cc2a54
seungbin9850 <tanja1206@naver.com>
2021-09-06 09:54:15 +0900

    [DEL] app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import connectionOptions from '../ormconfig';
-import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppService } from './app.service';
 import { UserModule } from './modules';
 
 @Module({
   imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
