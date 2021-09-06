0e58fe737c95ce459cad5343f0dcc786c764e8af
seungbin9850 <tanja1206@naver.com>
2021-09-06 09:55:38 +0900

    [FIX] path

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from 'src/repositories';
+import { UserRepository } from '../../repositories';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
