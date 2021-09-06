9f623141e8cd14e854c8276a1530f04676124a1a
seungbin9850 <tanja1206@naver.com>
2021-09-06 09:55:16 +0900

    [ADD] find user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/entities/user.entity';
-import { HttpError } from 'src/exception';
-import { UserRepository } from 'src/repositories';
-import { hash, makeId } from 'src/utils';
+import { User } from '../../entities/user.entity';
+import { HttpError } from '../../exception';
+import { UserRepository } from '../../repositories';
+import { hash, makeId } from '../../utils';
 import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
@@ -11,8 +11,8 @@ export class UserService {
 
   async createUser(req: CreateUserDTO) {
     const { userId, password } = req;
-    if (this.userRepository.findOne(userId))
-      throw new HttpError(409, 'Already Exist User ID');
+    if (await this.findUser(userId))
+      throw new HttpError(409, 'Already Exists User');
     const hashed = hash(password);
     const id = await makeId();
     const user = new User();
@@ -20,5 +20,11 @@ export class UserService {
     user.userId = userId;
     user.password = hashed;
     await this.userRepository.save(user);
+  }
+
+  private async findUser(userId: string) {
+    const user = await this.userRepository.findOneById(userId);
+    if (user) return true;
+    return false;
   }
 }
